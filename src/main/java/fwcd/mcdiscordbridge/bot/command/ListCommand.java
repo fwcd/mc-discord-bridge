2e578a36a3052c5178875e6c1a7b3e38ef7cf056
fwcd <fwcdmail@gmail.com>
2020-03-19 19:57:10 +0100

    Pluralize player count correctly in ListCommand

--- src/main/java/fwcd/mcdiscordbridge/bot/command/ListCommand.java
+++ src/main/java/fwcd/mcdiscordbridge/bot/command/ListCommand.java
@@ -3,6 +3,9 @@ package fwcd.mcdiscordbridge.bot.command;
 import net.dv8tion.jda.api.entities.Message;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import fwcd.mcdiscordbridge.utils.StringUtils;
+
 import java.util.Collection;
 import java.util.stream.Collectors;
 
@@ -15,7 +18,8 @@ public class ListCommand implements BotCommand {
         if (onlinePlayers.isEmpty()) {
             output = "No players online.";
         } else {
-            output = onlinePlayers.size() + " players online: "
+            int playerCount = onlinePlayers.size();
+            output = playerCount + " " + StringUtils.pluralize("player", playerCount) + " online: "
               + onlinePlayers.stream().map(p -> p.getPlayerListName()).collect(Collectors.joining(", "));
         }
 
