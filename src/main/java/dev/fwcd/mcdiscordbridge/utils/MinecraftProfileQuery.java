4f05a6edc2ef6256532f0a437dd6c1df47dda568
fwcd <fwcdmail@gmail.com>
2023-10-12 13:08:24 +0100

    Update profile query logging

--- src/main/java/dev/fwcd/mcdiscordbridge/utils/MinecraftProfileQuery.java
+++ src/main/java/dev/fwcd/mcdiscordbridge/utils/MinecraftProfileQuery.java
@@ -25,7 +25,7 @@ public class MinecraftProfileQuery {
     
     public UUID getUUIDSync() {
         try {
-            DiscordBridgeLogger.get().fine(() -> "Querying UUID for user '" + name + "'");
+            DiscordBridgeLogger.get().fine(() -> "Querying UUID for user `" + name + "`");
 
             String json = IOUtils.toString(new URL("https", "api.mojang.com", "/users/profiles/minecraft/" + name), StandardCharsets.UTF_8);
             Response response = GSON.fromJson(json, Response.class);
@@ -33,7 +33,7 @@ public class MinecraftProfileQuery {
 
             String uuidString = response.id.replaceAll("([0-9a-fA-F]{8})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]+)", "$1-$2-$3-$4-$5");
             UUID uuid = UUID.fromString(uuidString);
-            DiscordBridgeLogger.get().info(() -> "Got UUID " + uuid + " for '" + name + "'");
+            DiscordBridgeLogger.get().fine(() -> "Got UUID " + uuid + " for `" + name + "`");
             
             return uuid;
         } catch (IOException e) {
