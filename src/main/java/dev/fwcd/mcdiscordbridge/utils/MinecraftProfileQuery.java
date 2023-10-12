eb643a2d6996cdb2643dad56d8e96d37cafdbbfb
fwcd <fwcdmail@gmail.com>
2023-10-12 12:26:50 +0100

    Log more verbosely in MinecraftProfileQuery

--- src/main/java/dev/fwcd/mcdiscordbridge/utils/MinecraftProfileQuery.java
+++ src/main/java/dev/fwcd/mcdiscordbridge/utils/MinecraftProfileQuery.java
@@ -9,6 +9,8 @@ import java.util.concurrent.CompletableFuture;
 
 import com.google.gson.Gson;
 
+import dev.fwcd.mcdiscordbridge.plugin.DiscordBridgeLogger;
+
 import org.apache.commons.io.IOUtils;
 
 public class MinecraftProfileQuery {
@@ -26,9 +28,17 @@ public class MinecraftProfileQuery {
     
     public UUID getUUIDSync() {
         try {
+            DiscordBridgeLogger.get().fine(() -> "Querying UUID for user '" + name + "'");
+
             String json = IOUtils.toString(new URL("https", "api.mojang.com", "/users/profiles/minecraft/" + name), StandardCharsets.UTF_8);
             Response response = GSON.fromJson(json, Response.class);
-            return UUID.fromString(response.id.replaceAll("([0-9a-fA-F]{8})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]+)", "$1-$2-$3-$4-$5"));
+            DiscordBridgeLogger.get().fine(() -> "Got API response " + response);
+
+            String uuidString = response.id.replaceAll("([0-9a-fA-F]{8})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]{4})([0-9a-fA-F]+)", "$1-$2-$3-$4-$5");
+            UUID uuid = UUID.fromString(uuidString);
+            DiscordBridgeLogger.get().info(() -> "Got UUID " + uuid + " for '" + name + "'");
+            
+            return uuid;
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
